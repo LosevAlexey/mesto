c26484aa455e466f532c1d4e886d45b31d57bc38
Alexey Losev <zs-moskva@yandex.ru>
2023-04-01 00:39:58 +0300

    Fourth sprint 10

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 const container = document.querySelector('.popup');
 const editbutton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close');
-const nameInput = document.querySelector('.popup__name_box_empty');
-const descriptionInput = document.querySelector('.popup__description_box_empty');
-const saveButton = document.querySelector('.popup__save');
+const nameInput = document.querySelector('.popup__input_form_name');
+const descriptionInput = document.querySelector('.popup__input_form_description');
 const form = document.querySelector('.popup__form');
 const author = document.querySelector('.profile__author');
 const description = document.querySelector('.profile__description');
@@ -11,13 +10,12 @@ const description = document.querySelector('.profile__description');
 
 function openPopup() {
   container.classList.add("popup_opened");
-  nameInput.value = author.innerHTML;
-  descriptionInput.value = description.innerHTML;
+  nameInput.value = author.textContent;
+  descriptionInput.value = description.textContent;
 }
 
 function closePopup() {
   container.classList.remove("popup_opened");
-
 }
 
 
@@ -27,12 +25,7 @@ closeButton.addEventListener('click', closePopup);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  author.innerHTML = nameInput.value;
-  container.classList.remove("popup_opened");
-});
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  description.innerHTML = descriptionInput.value;
-  container.classList.remove("popup_opened");
+  author.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+  closePopup();
 });
