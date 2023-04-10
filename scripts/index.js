3b9ed3bfec6bbd7bb2a951717b6285fd3cd486bf
Alexey Losev <zs-moskva@yandex.ru>
2023-04-11 00:39:07 +0300

    Fifth sprint 1

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,15 @@ const author = document.querySelector('.profile__author');
 const description = document.querySelector('.profile__description');
 
 
+
 function openPopup() {
   container.classList.add("popup_opened");
+}
+
+function fullInput() {
   nameInput.value = author.textContent;
   descriptionInput.value = description.textContent;
+  openPopup();
 }
 
 function closePopup() {
@@ -19,7 +24,7 @@ function closePopup() {
 }
 
 
-editbutton.addEventListener('click', openPopup);
+editbutton.addEventListener('click', fullInput);
 closeButton.addEventListener('click', closePopup);
 
 
@@ -29,3 +34,116 @@ form.addEventListener('submit', (event) => {
   description.textContent = descriptionInput.value;
   closePopup();
 });
+
+
+
+const initialCards = [
+  {
+    name: 'Гостинный Двор',
+    image: './images/dvor.jpg'
+  },
+  {
+    name: 'Ростов-на-Дону',
+    image: './images/rostov.jpg'
+  },
+  {
+    name: 'Эльбрус',
+    image: './images/elbrus.jpg'
+  },
+  {
+    name: 'Коломна',
+    image: './images/kolomna.jpg'
+  },
+  {
+    name: 'Нальчик',
+    image: './images/nalchik.jpg'
+  },
+  {
+    name: 'Абрамцево',
+    image: './images/abramtsevo.jpg'
+  }
+];
+
+
+
+const placesTemplate = document.getElementById('places-template');
+const placesGrid = document.querySelector('.places');
+const addPlace = document.querySelector('.profile__add');
+const popupPlaces = document.querySelector('.popup-places');
+const popupImages = document.querySelector('.popup-more');
+const seeImage = document.querySelector('.places__image_more');
+const editNewPlace = document.querySelector('.popup__form_place');
+const buttonCloseAddPlace =document.querySelector('.popup__close-place');
+
+
+function createPlaceElement(placeDate) {
+  const placeElement = placesTemplate.content.querySelector('.places__block').cloneNode(true);
+  const placeImage = placeElement.querySelector('.places__image');
+  const placesTitle = placeElement.querySelector('.places__title');
+  placeImage.src = placeDate.image;
+  placeImage.alt = placeDate.name;
+  placesTitle.textContent = placeDate.name;
+  const deleteButton = placeElement.querySelector('.places__trash_delete-icon');
+  const likebutton = placeElement.querySelector('.places__like');
+  const handleDelete = () => {
+    placeElement.remove();
+  };
+  const handleLike = () => {
+    likebutton.classList.toggle('places__like_active');
+  };
+  deleteButton.addEventListener('click', handleDelete);
+  likebutton.addEventListener('click', handleLike);
+  return placeElement;
+}
+
+const renderPlaceElement = (placeElement) => {
+  placesGrid.prepend(placeElement);
+};
+
+initialCards.forEach((place) => {
+  renderPlaceElement(createPlaceElement(place));
+});
+
+function openPopupAddPlace() {
+  popupPlaces.classList.add("popup_opened");
+};
+
+function closePopupAddPlace() {
+  popupPlaces.classList.remove("popup_opened");
+};
+
+
+function popupAddPlace(event) {
+  event.preventDefault();
+  const namePlace = document.querySelector('.popup__input_place_description');
+  const linkPlace = document.querySelector('.popup__input_place_link');
+  const name = namePlace.value;
+  const image = linkPlace.value;
+  const placesDate = {
+    name,
+    image,
+  };
+  renderPlaceElement(createPlaceElement(placesDate));
+  closePopupAddPlace(editNewPlace);
+};
+
+editNewPlace.addEventListener("submit", popupAddPlace);
+buttonCloseAddPlace.addEventListener('click', closePopupAddPlace);
+
+function OpenPopupSeeImage() {
+  popupImages.classList.add("popup_opened");
+};
+
+function closePopupSeeImage() {
+  popupImages.classList.remove("popup_opened");
+};
+
+function seePopupSeeImage(event) {
+  event.preventDefault();
+  console.log();
+}
+
+buttonCloseAddPlace.addEventListener('click', closePopupSeeImage);
+
+addPlace.addEventListener('click', openPopupAddPlace);
+seeImage.addEventListener('click', OpenPopupSeeImage);
